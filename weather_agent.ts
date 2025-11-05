7fc6c53030f858de467d454641af7714cd5bc02c
Julien Kilo <julienkilo@gmail.com>
2025-11-05 17:53:00 +0100

    Refactor project to add 2 agents

--- weather_agent.ts
+++ weather_agent.ts
@@ -6,13 +6,18 @@ const model = "anthropic:claude-sonnet-4-5-20250929";
 
 const tools = [getWeather];
 
+const SYSTEM_PROMPT = `
+You are a weather assitant.
+
+Rules:
+- Kindly provide the weather to users
+- Call the tool \`getWeather\` when the user asks for the weather of a specific city.
+`;
+
 const agent = createAgent({
   model,
   tools,
+  systemPrompt: SYSTEM_PROMPT,
 });
 
-const response = await agent.invoke({
-  messages: [{ role: "user", content: "What's the weather in San Francisco?" }],
-});
-
-displayMessage(response.messages[response.messages.length - 1]);
+export default agent;
