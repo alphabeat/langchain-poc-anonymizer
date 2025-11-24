8b267d02724e3ff37515652b997438d228f44844
Julien Kilo <julienkilo@gmail.com>
2025-11-24 16:24:46 +0100

    Remove unused files

--- prompts/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as piiRedactionPrompt } from './piiRedaction.ts';
--- tools/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as executeSQL } from './execute-sql.ts';
-export { default as extractPII} from "./extract-pii.ts";
-export { default as getWeather } from './get-weather.ts';
